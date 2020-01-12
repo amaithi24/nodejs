6efe39abee7217b3ff3ca0e482c97e7866c4a77d
amaithi24 <59793035+amaithi24@users.noreply.github.com>
2020-01-12 19:45:14 +0530

    node

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
-var msg = 'Hello World';
-console.log(msg);
+var http = require('http');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World!');
+}).listen(8080);
