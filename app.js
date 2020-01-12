bd5be67a53a4bf4ac20ddba05868071b7e8cdfc0
amaithi24 <59793035+amaithi24@users.noreply.github.com>
2020-01-12 20:11:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
-var http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World!');
-}).listen(8080);
+// create express app
+const app = express();
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json())
+
+// define a simple route
+app.get('/', (req, res) => {
+    res.json({"message": "Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes."});
+});
+
+// listen for requests
+app.listen(3000, () => {
+    console.log("Server is listening on port 3000");
+});
